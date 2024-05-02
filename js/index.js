14d51765cf9a059ee457f7c502ad53031f10ed66
Stephanie Tjiong <stephanielaurentjiong@gmail.com>
2024-05-02 16:27:52 -0700

    form and functionality added

--- js/index.js
+++ js/index.js
@@ -27,11 +27,50 @@ const skillsList = skillsSection.querySelector('ul');
 for (let i = 0; i < skills.length; i++) {
     // create a list item for each skill
     const skill = document.createElement('ul');
-    // set each item from skills to the current skill
+    // set each item from skills to the current skill (baca reverse)
     skill.textContent = skills[i];
     //append skill element to skillList
     skillsList.appendChild(skill);
 }
+
+// const messageForm = document.querySelector("[name="leave_message"]");
+const messageForm = document.querySelector('form[name="leave_message"]');
+
+messageForm.addEventListener('submit', event => {
+    event.preventDefault();
+
+    // Callback for username
+    const username = event.target.usersName.value;
+    // Callback for usersEmail
+    const email = event.target.usersEmail.value;
+    // Callback for usersMessage
+    const message = event.target.usersMessage.value;
+
+    console.log("Name:", username);
+    console.log("Email:", email);
+    console.log("Message:", message);
+
+    const messageSection = document.querySelector('section#Messages');
+    const messageList = messageSection.querySelector('ul');
+    const newMessage = document.createElement('li');
+    newMessage.innerHtml = '<a href="mailto: ${usersEmail}">${usersName}</a> <span> ${usersMessage} </span>';
+
+    const removeButton = document.createElement('button');
+    removeButton.innertHtml = 'remove';
+    removeButton.type = 'button';
+    removeButton.addEventListener('click', event => {
+        const entry = removeButton.parentNode;
+
+        entry.remove();
+    });
+
+    newMessage.appendChild(removeButton);
+    messageList.appendChild(newMessage);
+
+    messageForm.reset();
+});
+
+
 
 body.appendChild(footerSect);
 
